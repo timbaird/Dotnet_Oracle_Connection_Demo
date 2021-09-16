d5cb192bbcd9baa41c704fed930a61c0ceb8cfb2
tbaird <tbaird@swin.edu.au>
2021-09-16 11:35:44 +1000

    Initial Blah

---
+++ Dotnet_Oracle_Connection_Sln/Dotnet_Oracle_Connection/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Dotnet_Oracle_Connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // connection
+            using (OracleConnection conn = new OracleConnection(Secret.GetConnString()))
+            {
+                using (OracleCommand comm = new OracleCommand())
+                {
+                    try
+                    {
+                        comm.Connection = conn;
+
+                        // double check you have data in the customer table
+                        // or change the query to look at a test table you have with data
+                        comm.CommandText = "Select * from customer";
+                        conn.Open();
+
+                        using( OracleDataReader reader = comm.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                //make sure the data types are correct here
+                                Console.WriteLine(reader.GetInt32(0) + " " +  reader.GetString(1));
+                            }
+                        }
+                    } 
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("ERROR: " + ex.Message);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+
+
+
+
+
+            }
+            // command
+            // command paremeter / types / 
+            // execute - execute non query / execute reader / 
+
+            // loop through the reader to 'use' the data (e.g. print to console)
+
+            // close the connection
+
+            // clean up / exception handling
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dotnet_Oracle_Connection_Sln/Dotnet_Oracle_Connection/Secret.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet_Oracle_Connection
+{
+    static class Secret
+    {
+        public static string GetConnString()
+        {
+            return  "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)" +
+                    "(HOST=feenix-oracle.swin.edu.au)(PORT=1521))" +
+                    "(CONNECT_DATA=(SERVICE_NAME=DMS)));" +
+                    "User Id=tbaird;Password=myPassword;";
+
+             /*
+             * Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=MyHost)(PORT=MyPort))
+             * (CONNECT_DATA=(SERVICE_NAME=MyOracleSID)));User Id=myUsername;Password=myPassword;
+             */
+        }
+
+    }
+}
